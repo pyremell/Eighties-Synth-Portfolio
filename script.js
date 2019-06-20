ed97dacfd29ae99dedbe827022bcc89cdd954b04
unknown <jacobschaefer12@gmail.com>
2019-06-20 12:11:58 -0700

    Built random quote generator for header, with API from http://movie-quotes-app.herokuapp.com/. Waiting on API access key.

---
+++ script.js
@@ -0,0 +1,27 @@
+const endpoint = 'http://movie-quotes-app.herokuapp.com/api/v1/quotes?category=sci-fi';
+
+function getQuote() {
+  fetch(endpoint)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      displayQuote(data.message);
+    })
+    .catch(function () {
+      console.log("An error occurred");
+    });
+}
+
+function displayQuote(quote) {
+  const quoteText = document.querySelector('.quote-text');
+  quoteText.textContent = quote;
+
+  const tweetButton = document.querySelector('.tweet');
+  tweetButton.setAttribute('href', `https://twitter.com/share?text=${quote}`);
+}
+
+const newQuoteButton = document.querySelector('.new-quote');
+newQuoteButton.addEventListener('click', getQuote);
+
+getQuote();
